fe01d3131f44a5461dd9a9c6ad59b586b80e1e5b
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-03-21 10:40:24 -0700

    WaterReservoir: has a default capacity of 10: passed

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,5 +1,8 @@
 require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
-
+    it 'has a default capacity of 10' do
+        reservoir = WaterReservoir.new
+        expect(reservoir.capacity).to eq(10)
+    end
 end
