51125def81f42aa9c22dc29bf66f0b906627aa05
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-03-21 11:03:16 -0700

    Freezer: has a default capacity of 100: passed

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -1,5 +1,8 @@
 require_relative '../lib/freezer'
 
 describe 'A freezer' do
-
+    it 'has a default capacity of 100' do
+        freezer = Freezer.new
+        expect(freezer.capacity).to eq(100)
+    end
 end
