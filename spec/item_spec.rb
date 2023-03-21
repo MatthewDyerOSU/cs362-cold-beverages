b29358dc08523bc7e5cc31b15e231c0e61ad68d8
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-03-21 10:36:11 -0700

    Item: has a volume: passed

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,8 +1,13 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
-    it 'has a name' do
+    it 'has a name when provided one' do
         item = Item.new('Test Name', 50)
         expect(item.name).to eq('Test Name')
+    end
+
+    it 'has a volume when provided one' do
+        item = Item.new('Test Name', 50)
+        expect(item.volume).to eq(50)
     end
 end
