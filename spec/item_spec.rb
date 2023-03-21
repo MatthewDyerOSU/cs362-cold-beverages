ae62b1da049675b0a5bd580f72952983305fc098
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-03-21 10:34:22 -0700

    Item: has a name: passed

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,8 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
-
+    it 'has a name' do
+        item = Item.new('Test Name', 50)
+        expect(item.name).to eq('Test Name')
+    end
 end
